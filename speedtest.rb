2d3c235581805a7c4504f4d2ae908ea9d9224796
Rodney Cullen <rodcul@gmail.com>
2016-03-29 20:04:00 +0700

    Added wifi ssid for linux

--- speedtest.rb
+++ speedtest.rb
@@ -1,17 +1,27 @@
-require "addressable/uri"
-require 'open-uri'
+require "net/http"
 
-# output = `speedtest-cli --simple`
-output = "Ping: 246.466 ms\nDownload: 2.69 Mbit/s\nUpload: 1.03 Mbit/s\n"
+output = `speedtest-cli --simple`
+# output = "Ping: 246.466 ms\nDownload: 2.69 Mbit/s\nUpload: 1.03 Mbit/s\n"
 ping = output[(output.index('Ping: ') + 6)..(output.index('ms')-2)]
 download = output[(output.index('Download: ') + 10)..(output.index('Mbit')-2)]
 upload = output[(output.index('Upload: ') + 8)..(output.length - 9)]
+
+# OSX vs. Linux
+# ssid = (`iwgetid -r`).strip
 ssid = (`/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | awk '/ SSID/ {print substr($0, index($0, $2))}'`).strip
 
-uri = Addressable::URI.new
-uri.query_values = {ping: ping, upload: upload, download: download, ssid: ssid}
-p uri.query
+uri = URI('http://speedy-cnx.herokuapp.com/speedtests')
 
-puts 'http://requestb.in/test?' + uri.query
-# response = open(URI.join('http://requestb.in/sjj8dfsj' , 'test'))
-# response.each_line { |f| f.each_line {|line| p line} }
+req = Net::HTTP::Post.new(uri)
+req.set_form_data({ping: ping, upload: upload, download: download, ssid: ssid})
+
+res = Net::HTTP.start(uri.hostname, uri.port) do |http|
+  http.request(req)
+end
+
+case res
+when Net::HTTPSuccess, Net::HTTPRedirection
+  # OK
+else
+  res.value
+end
