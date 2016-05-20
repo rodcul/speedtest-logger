4d90dd6264c1600cd2c69ce45054d0af7e256d0a
Rodney Cullen <rodcul@gmail.com>
2016-05-20 18:43:07 +0700

    Log ethernet if no ssid present

--- speedtest.rb
+++ speedtest.rb
@@ -20,8 +20,8 @@ begin
   ssid = %x[iwgetid -r].strip
 rescue
   ssid = (`/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | awk '/ SSID/ {print substr($0, index($0, $2))}'`).strip
-# ssid = (%x[iwgetid -r].strip == "") ? "en0" : %x[iwgetid -r].strip
 end
+ssid = "en0" if ssid == ""
 logger.debug(ssid)
 logger.debug('Getting hostname')
 hostname = Socket.gethostname
