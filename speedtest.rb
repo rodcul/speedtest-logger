bb5f99611b538ef852af0437c536e91a9461e35e
Rodney Cullen <rodcul@gmail.com>
2016-05-20 13:12:23 +0700

    Merged changes from pi

--- speedtest.rb
+++ speedtest.rb
@@ -1,25 +1,32 @@
-require "net/http"
+require 'net/http'
 require 'socket'
+require 'logger'
 
+logfile = File.open('/var/log/cronlog', 'a+')
+logger = Logger.new(logfile)
+logger.debug('Starting CRON')
+logger.debug('Running speedtest')
 output = `speedtest-cli --simple`
+
 # output = "Ping: 246.466 ms\nDownload: 2.69 Mbit/s\nUpload: 1.03 Mbit/s\n"
 ping = output[(output.index('Ping: ') + 6)..(output.index('ms')-2)]
 download = output[(output.index('Download: ') + 10)..(output.index('Mbit')-2)]
 upload = output[(output.index('Upload: ') + 8)..(output.length - 9)]
+logger.debug('Download speed: ' + download + ' Mbit/s')
 
 # OSX vs. Linux
-
+logger.debug('Getting SSID')
 begin
-  ssid = (`iwgetid -r`).strip
-  # iceberg!
+  ssid = %x[iwgetid -r].strip
 rescue
   ssid = (`/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | awk '/ SSID/ {print substr($0, index($0, $2))}'`).strip
-  # lifeboats
+# ssid = (%x[iwgetid -r].strip == "") ? "en0" : %x[iwgetid -r].strip
 end
-
-p ssid
+logger.debug(ssid)
+logger.debug('Getting hostname')
 hostname = Socket.gethostname
-
+logger.debug(hostname)
+logger.debug('Posting to API')
 uri = URI('http://speedy-cnx.herokuapp.com/speedtests')
 
 req = Net::HTTP::Post.new(uri)
@@ -31,7 +38,10 @@ end
 
 case res
 when Net::HTTPSuccess, Net::HTTPRedirection
-  # OK
+  logger.debug('API Success')
 else
   res.value
+  logger.debug('API Fail')
 end
+
+logger.debug('Finished CRON')
